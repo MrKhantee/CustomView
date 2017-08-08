239c07adea606d81403c34560a49c094a9ad060b
MrKhantee <mrkhantee@gmail.com>
2017-08-08 11:41:14 +0700

    Initial commit

---
+++ app/src/main/java/com/mrkhantee/customview1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mrkhantee.customview1;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
