aec4332e9bff5b6824a66ca6b8c31dae51e31b7f
MrKhantee <mrkhantee@gmail.com>
2017-08-08 11:54:20 +0700

    Create project

--- app/src/main/java/com/mrkhantee/customview1/MainActivity.java
+++ app/src/main/java/com/mrkhantee/customview1/MainActivity.java
@@ -3,12 +3,18 @@ package com.mrkhantee.customview1;
 import android.app.*;
 import android.os.*;
 
+// How to draw a circle in Android (onDraw method in View)
+// https://alvinalexander.com/android/how-to-draw-circle-in-android-view-ondraw-canvas
 public class MainActivity extends Activity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+		
+		setContentView(new CustomView(this));
+		setTitle("How to draw a circle in Android (onDraw method in View)");
+		
     }
 }
